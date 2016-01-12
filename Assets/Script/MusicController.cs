9dffd8745cc6ce9b118c08ab4ea10fff0beaaca3
MusoAga <m-oezdogan@live.de>
2016-01-12 20:57:53 +0100

    Musiccontroller hinzugefügt, bis jetzt nur eine musik

---
+++ Assets/Script/MusicController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+
+    public static MusicController instance = null;
+
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Awake () {
+
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Resources.Load<AudioClip>("Music/Normal3_Loop");
+        playMusic();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void playMusic()
+    {
+        audioSource.Play();
+    }
+}
