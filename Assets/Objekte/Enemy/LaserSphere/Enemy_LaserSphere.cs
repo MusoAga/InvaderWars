ced017c6b7d2ed24cff79804fd3136478f0f855f
BenjaminCe <benjamincelebi@gmx.de>
2016-01-19 16:43:36 +0100

    Lasersphere hinzugefügt, Gift-Planet hat nun alle Gegnertypen

---
+++ Assets/Objekte/Enemy/LaserSphere/Enemy_LaserSphere.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_LaserSphere : EnemyController_Base {
+
+	// Use this for initialization
+	void Start () {
+        transform.eulerAngles = new Vector3(0, 0, 225 - 90 * Random.Range(0, 2));
+	}
+
+    public override void enemyBehaviour()
+    {
+        moveInDirection(new Vector3(0, -1));
+    }
+
+    public override void onHit()
+    {
+
+    }
+}
---
+++ Assets/Objekte/Enemy/LaserSphere/LaserShot.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserShot : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.GetComponent<PlayerController_Base>() != null)
+            coll.gameObject.GetComponent<Hitable>().dealDamage(1);
+    }
+}
