7fad7913040219326ebb0d732ea5dae504a9397f
BenjaminCe <benjamincelebi@gmx.de>
2016-01-19 15:40:26 +0100

    Unbenutzte Gegnertypen (Shooter, Meteroids) entfernt, Level aktualisiert, Poisonshooter hinzugefügt

--- Assets/Objekte/Enemy/Poison/Enemy_PoisonShip.cs
+++ Assets/Objekte/Enemy/Poison/Enemy_PoisonShip.cs
@@ -6,10 +6,7 @@ public class Enemy_PoisonShip : EnemyController_Base
     
     public override void enemyBehaviour()
    {
-       //moveInDirection((-transform.up)*0.8f);
-       //moveInDirection((new Vector2(Random.value >= 0.5f ? (0.4f + Random.value) : -(0.7f + Random.value), -1)) * 0.8f);
-       moveInDirection(new Vector2(Mathf.Sin(Time.deltaTime), -1));
-        charge++;
+        base.enemyBehaviour();
         if (charge >= 30)
         {
             charge = 0;
